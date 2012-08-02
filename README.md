f82c4a1f21299ce9c07456849c97eabb874f21cc
Ran Tao <ran@ephexi.com>
2012-08-02 12:37:34 -0700

    Initial Commit with updated README

---
+++ README.md
@@ -0,0 +1,10 @@
+## This is a To Do list App
+## by Ran Tao & Alexis Finch
+
+It should do the following:
+1) Create a RESTful List resource with associated views.
+2) Create a RESTful Task resource with associated views. Each Task
+should belong to a single list.
+3) Tasks have a binary state: either uncompleted or completed.
+4) Build an API for the Lists and Tasks
+5) Build a command line client that consumes the API
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '7b59c7f43c91809cd4f26d1efb8bc466ae9ed067231a745bd9e4081fc51c76396ebbae1308dca41e7baa5dc2c45295d1f57d3536775621e0b098af848524fef8'
