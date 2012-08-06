bd894048a584f3717d4f0747c40a354a7acedff5
Ran Tao <ran@ephexi.com>
2012-08-05 19:24:19 -0700

    add create task functionality

--- lib/api_eater.rb
+++ lib/api_eater.rb
@@ -17,7 +17,8 @@ require 'rest_client'
 
 class ApiEater
 
-	TO_DO_HOST = "localhost:3000"
+	HOST = "localhost:3000"
+	@list_id = 1
 
 
 	def run
@@ -42,16 +43,35 @@ class ApiEater
 			  	puts ""
 			  	input = gets.chomp
 			  	puts ""
-			  	title = return_list_title(input)
+			  	title = get_list_title(input)
 			  	puts "The tasks list for #{title}:"
 			  	#call the get_list method to return the list of tasks associated with that list
 			  	get_tasks(input)
 			  	puts ""
 			  when "create"
 			  	#call the add_list method 
-
+			  	printf "What do you want to call your list?"
+			  	puts ""
+			  	title = gets.chomp
+			  	puts ""
+			  	create_new_list(title) 
+			  	printf "Your list: #{title} has been created"
+			  	puts ""
+			  	while command != "done"
+			  		printf "Add a task now or type 'done' if no more tasks to add"
+			  		puts ""
+			  		command = gets.chomp
+			  		if command != "done"
+				  		item = command
+				  		puts "List: #{title}"
+				  		get_last_list
+				  		create_new_task(item)
+				  		puts get_tasks(@list_id)
+				  		puts ""
+				  	end
+			  	end
 			  else
-			  	puts "pick either VIEW or CREATE please you lazy bum..."
+			  	puts "pick either 'view' or 'create' please you lazy bum..."
 			end
         end
 	end
@@ -64,46 +84,59 @@ class ApiEater
 	def get_lists
 		all_lists = RestClient.get(Addressable::URI.new({
 		    :scheme => "http",
-		    :host => TO_DO_HOST,
+		    :host => HOST,
 		    :path => "/lists.json"
 		  }).to_s)
 		parsed_lists = parse_response(all_lists)
-		return_lists_titles(parsed_lists)
+		get_lists_titles(parsed_lists)
 	end
 
-	def return_lists_titles(lists)
+	def get_lists_titles(lists)
 		lists.each do |list|
 			puts "#{list[:id]}. #{list[:title]}"
 		end
 		return ""
 	end
 
-	def return_list_title(list_id)
+	def get_list_title(list_id)
 		list = RestClient.get(Addressable::URI.new({
 		    :scheme => "http",
-		    :host => TO_DO_HOST,
+		    :host => HOST,
 		    :path => "/lists/#{list_id}.json"
 		  }).to_s)
 		parsed_list = parse_response(list)
 		parsed_list[:title]
 	end
 
+	def get_last_list
+		all_lists = RestClient.get(Addressable::URI.new({
+		    :scheme => "http",
+		    :host => HOST,
+		    :path => "/lists.json"
+		  }).to_s)
+		parsed_lists = parse_response(all_lists)
+		last_list = parsed_lists.last
+		@list_id = last_list[:id]
+		@list_title = last_list[:title]
+		return ""
+	end
+
 	def get_tasks(list_id)
 		all_tasks = RestClient.get(Addressable::URI.new({
 		    :scheme => "http",
-		    :host => TO_DO_HOST,
+		    :host => HOST,
 		    :path => "/tasks.json",
 		    :query_values => {
 		    	:list_id => list_id
 		    }
 		  }).to_s)
 		parsed_tasks = parse_response(all_tasks)
-		return_tasks_items(parsed_tasks)
+		get_tasks_items(parsed_tasks)
 	end
 
 
 
-	def return_tasks_items(tasks)
+	def get_tasks_items(tasks)
 		tasks.each do |task|
 			if task[:completed] 
 				status = "Done"
@@ -115,15 +148,45 @@ class ApiEater
 		return ""
 	end
 
+	def create_new_list(title)
+	  begin
+	    RestClient.post(Addressable::URI.new({
+	      :scheme => "http",
+	      :host => HOST,
+	      :path => "/lists.json"
+	    }).to_s, {
+	      :list => {
+	        :title => title
+	      }
+	    })
+	  rescue RestClient::UnprocessableEntity => e
+	    puts e.response
+	  end
+	end
+
+	def create_new_task(item)
+	  begin
+	    RestClient.post(Addressable::URI.new({
+	      :scheme => "http",
+	      :host => HOST,
+	      :path => "/tasks.json"
+	    }).to_s, {
+	      :task => {
+	        :item => item,
+	        :list_id => @list_id,
+	        :completed => false
+	      }
+	    })
+	  rescue RestClient::UnprocessableEntity => e
+	    puts e.response
+	  end
+	end
+
 	def task_belongs_to 
 		
 	end
 
 	def completed_tasks
-		
-	end
-
-	def add_list
 		
 	end
 
